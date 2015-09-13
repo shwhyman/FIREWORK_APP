d449aba51db6be98def5c9368a00ab3fe39817fe
shwhyman <feckdrink@btinternet.com>
2015-09-13 17:34:01 +0100

    Sends serial data to Arduino

--- Pyth_config_test.py
+++ Pyth_config_test.py
@@ -5,11 +5,18 @@ import wx.lib.scrolledpanel as scrolled
 from passlib.hash import sha256_crypt
 import collections
 from collections import OrderedDict
+import serial
+import time 
+import struct
 
 class Frame(wx.Frame):
     def __init__(self, title, config_file_name="config.yml"):
 
         wx.Frame.__init__(self, None, title=title, pos=(150,150), size=(455,400))
+
+	
+
+
         self.Bind(wx.EVT_CLOSE, self.OnClose)
 
 	self.somethings_changed = False
@@ -260,14 +267,17 @@ class Frame(wx.Frame):
             dlg.Destroy()
             if result == wx.ID_YES:
 	        Frame.Save(self)
+		#self.ser.close()
                 self.Destroy()   
 	    elif result == wx.ID_NO:
+		#self.ser.close()
 	        self.Destroy()
 	else:
 	    dlg = wx.MessageDialog(self, "Are you sure you want to quit the application?", "Confirm Exit", wx.YES_NO|wx.ICON_QUESTION)
             result = dlg.ShowModal()
             dlg.Destroy()
             if result == wx.ID_YES:
+		#self.ser.close()
                 self.Destroy()   
 
 
@@ -306,21 +316,21 @@ class FireGroupDisplay(wx.Panel):
 	group_channel_field = wx.StaticText(self, label=fire_group.current_channel)
 	group_channel_field.SetFont(font)
 
-	self.sub_grid_box.AddMany([(group_name_field,0, wx.EXPAND), (group_channel_field, 0, wx.EXPAND)])
+	self.sub_grid_box.AddMany([(group_name_field,2, wx.EXPAND), (group_channel_field, 1, wx.EXPAND)])
 
 	for sub_groups in fire_group.SubFireGroup_list:
 	    sub_name_field = wx.StaticText(self, label=sub_groups.subgroup_name)
 	    sub_channel_field = wx.StaticText(self, label=sub_groups.current_channel)
 	    sub_name_field.SetFont(font)
 	    sub_channel_field.SetFont(font)
-	    self.sub_grid_box.AddMany([(sub_name_field,0, wx.EXPAND), (sub_channel_field, 0, wx.EXPAND)])
+	    self.sub_grid_box.AddMany([(sub_name_field,2, wx.EXPAND), (sub_channel_field, 1, wx.EXPAND)])
 
-	self.main_grid_box.Add(self.sub_grid_box, proportion=0, flag=wx.EXPAND |wx.Left | wx.RIGHT)
+	self.main_grid_box.Add(self.sub_grid_box,3, flag=wx.EXPAND |wx.Left | wx.RIGHT)
 
 	self.status = wx.StaticText(self,label='STBY')
 	self.status.SetFont(font)
 
-	self.main_grid_box.Add(self.status,2,wx.EXPAND)
+	self.main_grid_box.Add(self.status,1,wx.EXPAND)
 
 	self.SetSizerAndFit(self.main_grid_box)
 
@@ -332,6 +342,12 @@ class FireWindow(wx.Frame):
     def __init__(self, parent, config, config_file_name):
 	wx.Frame.__init__(self, parent, title = "Fire Sequence")
 
+	self.parent = parent
+
+	self.ser = serial.Serial('/dev/ttyACM0', 9600)	
+	
+	time.sleep(2)
+	      
 
 	self.ShowFullScreen(True)
 	self.config = config
@@ -342,8 +358,14 @@ class FireWindow(wx.Frame):
 	self.panel.Unbind(wx.EVT_SET_FOCUS)
 	self.panel.Unbind(wx.EVT_KILL_FOCUS)
 
-	self.panel.Bind(wx.EVT_KEY_DOWN, self.OnKey)
-	#self.panel.Bind(wx.EVT_KEY_UP, self.OnKey)
+	
+	fire_id = wx.NewId()
+	abort_id = wx.NewId()
+	self.Bind(wx.EVT_MENU, self.OnFire,id=fire_id)
+	self.Bind(wx.EVT_MENU, self.OnAbort, id=abort_id)
+	
+	self.accel_table = wx.AcceleratorTable([(wx.ACCEL_SHIFT | wx.ACCEL_CTRL, wx.WXK_RETURN, fire_id), (wx.ACCEL_SHIFT | wx.ACCEL_CTRL, wx.WXK_ESCAPE, abort_id)])
+	self.SetAcceleratorTable(self.accel_table)
 		
 
 	self.box = wx.BoxSizer(wx.VERTICAL)
@@ -354,14 +376,15 @@ class FireWindow(wx.Frame):
 	
 	#---SETUP FIRE LIST---#
 
+
 	for groups in parent.FireGroup_list:
 
 	    new_display = FireGroupDisplay(self, -1, groups)
-	    self.box.Add(new_display, border=20)
+	    self.box.Add(new_display, proportion=1,  border=20, flag=wx.EXPAND | wx.LEFT | wx.RIGHT)
 	    self.FireGroups.append(new_display)
 
 
-	self.panel.SetSizer(self.box)
+	self.panel.SetSizerAndFit(self.box)
         self.panel.SetFocus()
 
 	self.panel.Layout()
@@ -369,52 +392,67 @@ class FireWindow(wx.Frame):
 	#self.panel.SetupScrolling(scrollToTop=False)
 	
 
-    def OnKey(self, evt):
-
-	key_code = evt.GetKeyCode()
-	if key_code == wx.WXK_ESCAPE:	
-
-	    dlg = wx.PasswordEntryDialog(self, "Please confirm your password to abort the firing system:", "Password Verification Required!")
-	    result = dlg.ShowModal()
-	    password_attempt = dlg.GetValue()
-	    outcome = sha256_crypt.verify(password_attempt, self.config["password"])
-	    dlg.Destroy()
-	    if result != wx.ID_CANCEL:
-	        if outcome == False:
-	            error_dlg = wx.MessageDialog(self, "The password you entered is incorrect!", "Incorrect Password!", wx.OK | wx.ICON_WARNING)
-	            error_dlg.ShowModal()
-	            error_dlg.Destroy()
-	        elif outcome == True:
-	            caution_dlg = wx.MessageDialog(self, "Are you sure you wish to abort the firing sequence?", "Abort Firing Sequence", wx.YES_NO | wx.ICON_INFORMATION)
-	            result = caution_dlg.ShowModal()
-	            caution_dlg.Destroy()
-	            if result == wx.ID_YES:
-		        yes_dlg = wx.MessageDialog(self, "The firing sequence was successfully aborted.", "Sequence Aborted", wx.OK | wx.ICON_INFORMATION)	
-			yes_dlg.ShowModal()
-	                self.MakeModal(False)
-		        self.Destroy()
+    def OnAbort(self, evt):
 	
-	elif key_code == wx.WXK_RETURN:
-
-	    if self.counter == len(self.FireGroups):
-		self.FireGroups[self.counter - 1].SetBackgroundColour('#d3d3d3')
-		finish_dlg = wx.MessageDialog(self, "The firing sequence has finished, and will now exit.", "End Of Sequence", wx.OK | wx.ICON_INFORMATION)
-		finish_dlg.ShowModal()
-		self.MakeModal(False)
-		self.Destroy()
-		return
+	dlg = wx.PasswordEntryDialog(self, "Please confirm your password to abort the firing system:", "Password Verification Required!")
+	result = dlg.ShowModal()
+	password_attempt = dlg.GetValue()
+	outcome = sha256_crypt.verify(password_attempt, self.config["password"])
+	dlg.Destroy()
+	if result != wx.ID_CANCEL:
+	    if outcome == False:
+	        error_dlg = wx.MessageDialog(self, "The password you entered is incorrect!", "Incorrect Password!", wx.OK | wx.ICON_WARNING)
+	        error_dlg.ShowModal()
+	        error_dlg.Destroy()
+	    elif outcome == True:
+	        caution_dlg = wx.MessageDialog(self, "Are you sure you wish to abort the firing sequence?", "Abort Firing Sequence", wx.YES_NO | wx.ICON_INFORMATION)
+	        result = caution_dlg.ShowModal()
+	        caution_dlg.Destroy()
+	        if result == wx.ID_YES:
+		    yes_dlg = wx.MessageDialog(self, "The firing sequence was successfully aborted.", "Sequence Aborted", wx.OK | wx.ICON_INFORMATION)	
+		    yes_dlg.ShowModal()
+	            self.MakeModal(False)
+		    self.Destroy()
+	
+    def OnFire(self,evt):
+	
+        if self.counter == len(self.FireGroups):
+	    self.FireGroups[self.counter - 1].SetBackgroundColour('#d3d3d3')
+	    finish_dlg = wx.MessageDialog(self, "The firing sequence has finished, and will now exit.", "End Of Sequence", wx.OK | wx.ICON_INFORMATION)
+	    finish_dlg.ShowModal()
+	    self.MakeModal(False)
+	    self.Destroy()
+	    return
  
-	    if self.FireGroups[self.counter].status.GetLabel() == 'STBY':
-	        self.FireGroups[self.counter].status.SetLabel('ARMED')	
-	        self.FireGroups[self.counter].status.SetForegroundColour('#FF0000')
-		if self.counter != 0:
-		    self.FireGroups[self.counter - 1].SetBackgroundColour('#d3d3d3')
-	    elif self.FireGroups[self.counter].status.GetLabel() == 'ARMED':
-		self.FireGroups[self.counter].status.SetLabel('FIRED')
-	        self.FireGroups[self.counter].status.SetForegroundColour('#000000')
-		self.FireGroups[self.counter].SetBackgroundColour('#FF0000')
+	if self.FireGroups[self.counter].status.GetLabel() == 'STBY':
+	    self.FireGroups[self.counter].status.SetLabel('ARMED')	
+	    self.FireGroups[self.counter].status.SetForegroundColour('#FF0000')
+	    if self.counter != 0:
+		self.FireGroups[self.counter - 1].SetBackgroundColour('#d3d3d3')
+	elif self.FireGroups[self.counter].status.GetLabel() == 'ARMED':
+	    self.FireGroups[self.counter].status.SetLabel('FIRED')
+
+	    channel_list = []
+
+	    channel_list.append(self.parent.FireGroup_list[self.counter].current_channel) 
+	    for subs in self.parent.FireGroup_list[self.counter].SubFireGroup_list:
+		channel_list.append(subs.current_channel)
+
+	    send_string = ''
+	    
+	    for i in range(0, len(channel_list)):
+		if (i > 0):
+		    send_string = send_string + ','
+		send_string = send_string + channel_list[i]
+
+	   		
+
+	    self.ser.write(send_string.encode("utf-8")) 
+
+	    self.FireGroups[self.counter].status.SetForegroundColour('#000000')
+	    self.FireGroups[self.counter].SetBackgroundColour('#FF0000')
 	  	
-	        self.counter += 1 
+	    self.counter += 1 
 
 		             
 	
